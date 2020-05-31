162d92a79c75fd3757a2e48a3514967964cdd33f
iamkhattar <shivamkhattar1@gmail.com>
2020-06-01 00:04:57 +0100

    Landing Complete

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,15 +8,48 @@ const Landing = () => {
     <div className="mainApp landingPage">
       <div className="container h-100">
         <div className="h-25"></div>
-        <div className="row align-items-center h-25 justify-content-center mb-5">
-          <div className="col-8">
+        <div className="row align-items-center h-25 justify-content-center">
+          <div className="col-9">
             <img src={require("../../assets/fortnite-logo.png")} width="100%" />
           </div>
         </div>
         <div className="row align-items-center button-height justify-content-center mt-5">
-          <div className="col-2 h-100">Leaderboard</div>
-          <div className="col-2 h-100">Streams</div>
-          <div className="col-2 h-100">Add Streams</div>
+          <div className="col-3 h-100">
+            <button className="landing-button">
+              <Link
+                className="landing-button-text w-100 p-3"
+                to="/addstreams"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                ADD STREAMS
+              </Link>
+            </button>
+          </div>
+          <div className="col-3 h-100">
+            <button className="landing-button">
+              <Link
+                className="landing-button-text w-100 p-3"
+                to="/streams"
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                }}
+              >
+                STREAMS
+              </Link>
+            </button>
+          </div>
+          <div className="col-3 h-100">
+            <button className="landing-button">
+              <Link
+                className="landing-button-text w-100 p-3"
+                to="/leaderboard"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                LEADERBOARD
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
     </div>
