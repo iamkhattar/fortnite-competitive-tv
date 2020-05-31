3dadb40b1847cb396617eed1ab5395544f851f52
iamkhattar <shivamkhattar1@gmail.com>
2020-05-31 22:29:09 +0100

    Electron Setup

--- public/electron.js
+++ public/electron.js
@@ -4,33 +4,43 @@ const path = require("path");
 const isDev = require("electron-is-dev");
 
 let mainWindow = null;
-app.on("ready", createWindow);
-app.on("window-all-closed", function () {
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-app.on("activate", function () {
+
+app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
 });
-function createWindow() {
+
+const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 1024,
     title: "Chat desktop app demo",
   });
+
   mainWindow.maximize();
+
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
-  mainWindow.on("closed", function () {
+
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
-  mainWindow.on("page-title-updated", function (e) {
+
+  mainWindow.on("page-title-updated", (e) => {
     e.preventDefault();
   });
-}
+};
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Landing.css";
 
+import { Link } from "react-router-dom";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <Link to="/addstreams">AddStreams</Link>
+    </div>
+  );
 };
 export default Landing;
