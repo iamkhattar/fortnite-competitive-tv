760eb4af580fb84e14d959d7836d7ae947ef8ef2
iamkhattar <shivamkhattar1@gmail.com>
2020-06-02 19:46:34 +0100

    add Streams

--- src/components/AddStreams/AddStreams.js
+++ src/components/AddStreams/AddStreams.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddStreams.css";
 
+import { useStoreState, useStoreActions } from "easy-peasy";
 const AddStreams = () => {
-  return <div>AddStreams</div>;
+  const usernames = useStoreState((state) => state.usernames);
+  const remove = useStoreActions((actions) => actions.remove);
+  const [newStream, setNewStream] = useState("");
+  return (
+    <div className="mainApp">
+      <ul className="list-group">
+        {usernames &&
+          usernames.map((username) => {
+            return (
+              <button
+                className="row-height list-group-item list-group-item-action list-group-item-dark"
+                onClick={() => remove(username)}
+                key={username}
+              >
+                {username}
+                <span className="float-right">x</span>
+              </button>
+            );
+          })}
+      </ul>
+      <div>
+        <form
+          onSubmit={() => {
+            setNewStream("");
+          }}
+        >
+          <input
+            type="text"
+            className="row-height w-100 p-3 fixed-bottom"
+            placeholder="Twitch Username"
+            value={newStream}
+            onChange={(e) => setNewStream(e.target.value)}
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 export default AddStreams;
--- src/model.js
+++ src/model.js
@@ -1,12 +1,21 @@
 import { action } from "easy-peasy";
 
 export default {
-  usernames: ["tfue"],
-  add: action(
-    (state, username) => (state.usernames = [...state.usernames, username])
-  ),
-  remove: action(
-    (state, username) =>
-      (state.usernames = state.usernames.filter((uname) => uname !== username))
-  ),
+  usernames: [
+    "tfue1",
+    "tfue2",
+    "tfue3",
+    "tfue4",
+    "tfue5",
+    "tfue6",
+    "tfue7",
+    "tfue8",
+    "tfue9",
+  ],
+  add: action((state, username) => {
+    state.usernames = [...state.usernames, username];
+  }),
+  remove: action((state, username) => {
+    state.usernames = state.usernames.filter((uname) => uname !== username);
+  }),
 };
