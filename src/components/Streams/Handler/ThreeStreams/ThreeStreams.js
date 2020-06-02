5c0a9e38c58d70a5524fe7e89f05bd0a91d6141b
iamkhattar <shivamkhattar1@gmail.com>
2020-06-02 18:54:37 +0100

    ThreeStreams Completed

--- src/components/Streams/Handler/ThreeStreams/ThreeStreams.js
+++ src/components/Streams/Handler/ThreeStreams/ThreeStreams.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./TwoStreams.css";
-const TwoStream = ({ stream1, stream2 }) => {
+import "./ThreeStreams.css";
+
+const ThreeStreams = ({ stream1, stream2, stream3 }) => {
   const url1 = "https://player.twitch.tv/?channel=" + stream1;
   const url2 = "https://player.twitch.tv/?channel=" + stream2;
+  const url3 = "https://player.twitch.tv/?channel=" + stream2;
   return (
     <div className="mainApp">
-      <div className="row h-50 w-100">
+      <div className="row row-height w-100 m-0 p-0">
         <iframe
           src={url1}
           height="100%"
@@ -15,9 +17,19 @@ const TwoStream = ({ stream1, stream2 }) => {
           allowfullscreen={true}
         />
       </div>
-      <div className="row h-50 w-100">
+      <div className="row row-height w-100 m-0 p-0">
         <iframe
           src={url2}
+          height="100%"
+          width="100%"
+          frameborder="0"
+          scrolling={false}
+          allowfullscreen={true}
+        />
+      </div>
+      <div className="row row-height w-100 m-0 p-0">
+        <iframe
+          src={url3}
           height="100%"
           width="100%"
           frameborder="0"
@@ -29,4 +41,4 @@ const TwoStream = ({ stream1, stream2 }) => {
   );
 };
 
-export default TwoStream;
+export default ThreeStreams;
--- src/components/Streams/Streams.js
+++ src/components/Streams/Streams.js
@@ -3,15 +3,23 @@ import "./Streams.css";
 import OneStream from "./Handler/OneStream/OneStream";
 import NoStream from "./Handler/NoStream/NoStream";
 import TwoStream from "./Handler/TwoStreams/TwoStreams";
+import ThreeStreams from "./Handler/ThreeStreams/ThreeStreams";
 
 const Streams = () => {
-  const [usernames, setUserNames] = useState(["tfue", "tsm_zexrow"]);
+  const [usernames, setUserNames] = useState(["tfue", "tsm_zexrow", "tfue"]);
   return (
     <div className="mainApp">
       {usernames.length === 0 ? <NoStream /> : null}
       {usernames.length === 1 ? <OneStream stream1={usernames[0]} /> : null}
       {usernames.length === 2 ? (
         <TwoStream stream1={usernames[0]} stream2={usernames[1]} />
+      ) : null}
+      {usernames.length === 3 ? (
+        <ThreeStreams
+          stream1={usernames[0]}
+          stream2={usernames[1]}
+          stream3={usernames[2]}
+        />
       ) : null}
     </div>
   );
